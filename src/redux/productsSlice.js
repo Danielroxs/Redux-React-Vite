0edb6c37baf43275b262866eefacf0960a3ec59c
Danielroxs <danroxsll@gmail.com>
2024-06-26 20:46:37 -0600

    CRUD create, read, update

--- src/redux/productsSlice.js
+++ src/redux/productsSlice.js
@@ -2,11 +2,27 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const productsSlice = createSlice({
     name: "products",
-    initialState: [],
+    initialState: {
+        data: []
+    },
     reducers: {
-        createProduct: (state, action) => { },
-        readProduct: (state, action) => { },
-        updateProduct: (state, action) => { },
+        createProduct: (state, action) => {
+            state.data.push(action.payload)
+        },
+
+        readProduct: (state, action) => {
+            state.data = action.payload
+        },
+
+        updateProduct: (state, action) => {
+            const { id, name } = action.payload;
+            const product = state.data.find((product) => product.id === id);
+
+            if (product) {
+                product.name = name;
+            }
+        },
+
         deleteProduct: (state, action) => { }
     }
 })
